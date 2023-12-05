3a4eb031fe8a5312dc49752b31a3b22688d3dc03
Jan-Loettgen <janlucaloettgen@gmail.com>
2023-12-05 15:28:21 +0000

    julia day 5 part 1

--- src/day5.jl
+++ src/day5.jl
@@ -1,41 +1,52 @@
-f = open("/home/luca/Desktop/fun/AoC2023/inputs/day5.txt", "r")
- 
-# do some file operations
 
-# to count total lines in the file
-line_count = 0             
-
-# for line in readlines(f)
-
-
-# end
-lines = readlines(f) 
+@time begin
+f = open("/home/bucketking/Documents/fun/AoC2023/inputs/day5.txt", "r")
 
 j= 0
 mappings = []
-for (i, line) in enumerate(lines)
-    #println(line)
+for (i, line) in enumerate(readlines(f) )
 
     if i == 1
-        seeds = [parse(Int64, a) for a in split(line)[2:end]]
+        global seeds = [parse(Int64, a) for a in split(line)[2:end]]
     end
 
-    if line == "\n"
-        mappings.append!([])
-        j += 1
-    elseif isnumeric(line[0])
-        mappings[j].append!([parse(Int64, a) for a in split(line, " ")])
-        
+    if length(line) == 0
+        push!(mappings, [])
+        global j += 1
+    elseif isnumeric(line[1])
+        push!(mappings[j], [parse(Int64, a) for a in split(line, " ")])
     end
-
-
 end
-#print(lines)
-#print(split(lines,"\n"))
+
+seeds2 = Array{Int64}(undef, length(seeds))
+for (i, seed) in enumerate(seeds)
+    val = seed
+    for maping in mappings
+        for map in maping
+            if (val >= map[2]) &&  (val < map[2]+map[3])
+                val = map[1] +  (val-map[2])
+            break
+            end
+        end
+    end
+    seeds2[i] = val
+end
+
+println(minimum(seeds2))
+ranges = []
+
+for i in range(0, length(seeds)/2 - 1)
+    push!(ranges, (seeds[Int64((2*i) + 1)], seeds[Int64((2*i) + 1)] + seeds[Int64((2*i) + 2)]))
+end
+
+# range = (0, max_seed)
 
 
-# close the file instance
+
+#println(seeds)
+#println(max_seeds)
+
 close(f)
-
+end
 
 
